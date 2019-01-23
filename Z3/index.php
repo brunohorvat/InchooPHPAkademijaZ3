ca0cedbb417421c87bfde45ef6ec2aff2c4c5149
Bruno Horvat <46749189+brunohorvat@users.noreply.github.com>
2019-01-23 01:59:37 +0100

    Add files via upload

---
+++ Z3/index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!doctype html>
+<html lang="en" xml:lang="en">
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  <form action="table.php" method="post">
+      X: <input type="text" name="X"><br>
+      Y: <input type="text" name="Y"><br>
+      <input type="submit">
+  </form>
+
+  </body>
+</html>
---
+++ Z3/table.php
@@ -0,0 +1,7 @@
+<?php
+
+$brojevi= $_POST["X"];
+echo $brojevi;
+
+$brojevi1= $_POST["Y"];
+echo $brojevi1;
