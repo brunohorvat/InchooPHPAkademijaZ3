4c3262f03c7868b1f985c0a263bea94dc9a7da3b
Bruno Horvat <46749189+brunohorvat@users.noreply.github.com>
2019-01-24 04:51:18 +0100

    Add files via upload

--- Z3/index.php
+++ Z3/index.php
@@ -3,25 +3,45 @@
 <!doctype html>
 <html lang="en" xml:lang="en">
   <head>
-    <title></title>
+    <title>3. Zadaća</title>
   </head>
   <body>
 
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">-->
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>-->
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>-->
+<!---->
+<br>
+<br>
+<br>
+<div>
+      <form action="" method="post">
+          Broj redaka:<br> <input type="text" name="X"><br>
+          Broj stupaca: <br><input type="text" name="Y"><br>
+          <input type="submit" value="KREIRAJ TABLICU" class="btn btn-success">
+      </form>
+</div>
 
-  <form action="" method="post">
-      X: <input type="text" name="X"><br>
-      Y: <input type="text" name="Y"><br>
-      <input type="submit" value="KREIRAJ TABLICU">
-  </form>
 
-  <table >
+
+
+  <table>
       <link rel="stylesheet" href="stylesheet.css">
       <tbody>
+
   <?php
 
-
-
+if(($_POST['X'])<=10)
+{
   printSpiral($_POST['X']);
+}
+elseif(($_POST['X'])>10){
+    echo "<script type='text/javascript'>alert('Unesite broj manji ili jednak 10')</script>";
+
+    printSpiral(5);
+}
 
 
 function printSpiral($n)
@@ -61,6 +81,7 @@ $var1=(($n*$n)+1)-(($n - 2 * $x) * ($n - 2 * $x) - ($i - $x) - ($j - $x));
   ?>
       </tbody>
   </table>
+
 
 
   </body>
